a8e16698e8df90c8aee6ffd90dd4679b91399013
Priyank Patel <patel.priyank@gmail.com>
2014-05-27 17:18:08 -0400

    final implementation

--- assignments/week-1-assignment-0-v2/src/edu/vuum/mooca/SynchronizedQueueImpl.java
+++ assignments/week-1-assignment-0-v2/src/edu/vuum/mooca/SynchronizedQueueImpl.java
@@ -1,5 +1,7 @@
 package edu.vuum.mooca;
 
+import java.io.IOException;
+
 /**
  * @class SynchronizedQueueImpl
  *
@@ -17,29 +19,77 @@ public class SynchronizedQueueImpl extends SynchronizedQueue {
     // TODO - change this to true if you want to see diagnostic
     // output on the console as the test runs.
     static {
-        diagnosticsEnabled = false;
+        diagnosticsEnabled = true;
     }
 	
     protected void createThreads() {
         // TODO - replace the "null" assignments below to create two
         // Java Threads, one that's passed the mProducerRunnable and
         // the other that's passed the mConsumerRunnable.
-        mConsumer = null;
-        mProducer = null;
+        mConsumer = new Thread(new ProducerRunnable());
+        mProducer = new Thread(new ConsumerRunnable());
     }
     
     protected void startThreads() {
         // TODO - you fill in here to start the threads. More
         // interesting results will occur if you start the
         // consumer first.
+    	mProducer.start();
+    	mConsumer.start();
     }
 
     protected void interruptThreads() {
         // TODO - you fill in here to interrupt the threads.
+    	if(mConsumer != null)
+    		mConsumer.interrupt();
+    	
+    	if(mProducer != null)
+    		mProducer.interrupt();
     }
 
     protected void joinThreads() throws InterruptedException {
         // TODO - you fill in here to wait for the threads to
         // exit.
+    	if(mConsumer != null)
+    		mConsumer.join();
+    	
+    	if(mProducer != null)
+    		mProducer.join();
     }
+    
+    public class ProducerRunnable implements Runnable {
+
+        public void run() {
+        	
+        	/* I implemented interruption handling through Thread.currentThread().isInterrupted() but below are other options too
+        	 * Option 2: I could have also implemented this through boolean 'volatile' properties and checking its value in loop
+        	 * Option 3: If ProducerRunnable was implemented by extending from the 'Thread' class, it was sufficient to call isInterrupted instead of Thread.currentThread().isInterrupted() 
+        	 */
+        	while (!Thread.currentThread().isInterrupted()) {
+                try {
+                	System.out.println("Hello from a Producer!");                	
+                } catch (Exception ex) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+    
+    public class ConsumerRunnable implements Runnable {
+
+        public void run() {
+        	/* I implemented interruption handling through Thread.currentThread().isInterrupted() but below are other options too
+        	 * Option 2: I could have also implemented this through boolean 'volatile' properties and checking its value in loop
+        	 * Option 3: If ProducerRunnable was implemented by extending from the 'Thread' class, it was sufficient to call isInterrupted instead of Thread.currentThread().isInterrupted() 
+        	 */
+        	while (!Thread.currentThread().isInterrupted()) {
+                try {
+                	System.out.println("Hello from a Consumer!");                	
+                } catch (Exception ex) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
 }            
