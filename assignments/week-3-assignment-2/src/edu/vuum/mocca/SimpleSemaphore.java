6012b3ce873805937ece7c6111a7a8a19a73d7c6
priyank patel <patel.priyank@gmail.com>
2014-06-28 16:50:05 -0400

    week 3 : work in progress

--- assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
+++ assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -1,3 +1,142 @@
+<<<<<<< HEAD
+// Import the necessary Java synchronization and scheduling classes.
+
+package edu.vuum.mocca;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @class SimpleAtomicLong
+ *
+ * @brief This class implements a subset of the
+ *        java.util.concurrent.atomic.SimpleAtomicLong class using a
+ *        ReentrantReadWriteLock to illustrate how they work.
+ */
+class SimpleAtomicLong
+{
+    /**
+     * The value that's manipulated atomically via the methods.
+     */
+    private long mValue;
+
+
+    /**
+     * The ReentrantReadWriteLock used to serialize access to mValue.
+     */
+    // TODO - add the implementation
+    private ReentrantReadWriteLock reRWLock;
+
+    /**
+     * Creates a new SimpleAtomicLong with the given initial value.
+     */
+    public SimpleAtomicLong(long initialValue) {
+        // TODO - you fill in here
+    	mValue = initialValue;
+    	reRWLock = new ReentrantReadWriteLock();
+    }
+
+    /**
+     * @brief Gets the current value
+     * 
+     * @returns The current value
+     */
+    public long get() {
+        // TODO - you fill in here
+    	reRWLock.readLock().lock();
+    	try
+    	{
+    		return mValue;
+    	}
+    	finally{
+    		reRWLock.readLock().unlock();
+    	}
+    }
+
+    /**
+     * @brief Atomically decrements by one the current value
+     *
+     * @returns the updated value
+     */
+    public long decrementAndGet() {
+        // TODO - you fill in here
+    	reRWLock.writeLock().lock();
+    	try
+    	{
+    		mValue--;
+    		
+    		return mValue;
+    	}
+    	finally{
+    		reRWLock.writeLock().unlock();
+    	}    	
+    }
+
+    /**
+     * @brief Atomically increments by one the current value
+     *
+     * @returns the previous value
+     */
+    public long getAndIncrement() {
+        // TODO - you fill in here
+    	
+    	long previousValue = get();
+    	
+    	reRWLock.writeLock().lock();
+    	try
+    	{
+    		mValue++;
+    	}
+    	finally{
+    		reRWLock.writeLock().unlock();
+    	}
+    	
+    	return previousValue;
+    }
+
+    /**
+     * @brief Atomically decrements by one the current value
+     *
+     * @returns the previous value
+     */
+    public long getAndDecrement() {
+        // TODO - you fill in here
+
+    	long previousValue = get();
+    	
+    	reRWLock.writeLock().lock();
+    	try
+    	{
+    		mValue--;
+    	}
+    	finally{
+    		reRWLock.writeLock().unlock();
+    	}
+    	
+    	return previousValue;
+    }
+
+    /**
+     * @brief Atomically increments by one the current value
+     *
+     * @returns the updated value
+     */
+    public long incrementAndGet() {
+        // TODO - you fill in here
+    	reRWLock.writeLock().lock();
+    	try
+    	{
+    		mValue++;
+    		return mValue;
+    	}
+    	finally{
+    		reRWLock.writeLock().unlock();
+    	}
+    }
+}
+
+
+=======
 package edu.vuum.mocca;
 
 import java.util.concurrent.locks.ReentrantLock;
@@ -68,3 +207,4 @@ public class SimpleSemaphore {
         return null;
     }
 }
+>>>>>>> 88c1b9e84cab72f338826ee5a2142579ef30c501
